dfb28517ce4cd3f8c77c3933a00145a78af583c2
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-12-10 15:08:32 +0200

    Software dev get posts

--- src/main/java/com/example/spring_api/SoftwareEngineerController.java
+++ src/main/java/com/example/spring_api/SoftwareEngineerController.java
@@ -1,4 +1,20 @@
 package com.example.spring_api;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareDeveloper> getSoftwareEngineers(){
+        return List.of(
+                new SoftwareDeveloper(1, "zaden", "js, python, react"),
+                new SoftwareDeveloper(2, "Bolo", "js, c#, Angular")
+        );
+    }
 }
