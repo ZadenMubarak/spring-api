50852365b6e6ae868abc051e3d9004c37e9b49f4
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-12-27 23:26:39 +0200

    special shell instructions

--- postgress-commands.sh
+++ postgress-commands.sh
@@ -8,3 +8,6 @@ echo "ps"
 docker compose ps
 echo "entering special bash"
 docker exec -it postgres bash
+
+echo "once in the special shell copy this command: psql -U postgress"
+echo "Change the postgres to your actual postgres username"
